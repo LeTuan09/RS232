d7cdcbe65b72bd183abe4fb6b372a7fa73900d67
LeTuan09 <lengoctuan0105@gmail.com>
2024-02-23 18:04:57 +0700

    init

--- index.js
+++ index.js
@@ -24,24 +24,9 @@ const ReadDataString = (port, callback) => {
         })
 }
 
-
 module.exports = {
     connectSerialPort,
     ReadDataString
 }
 
 
-// const port = new SerialPort({ path: 'COM4', baudRate: 9600, parity: "none", dataBits: 8, stopBits: 1, flowControl: false });
-// const parser = port.pipe(new ReadlineParser({ decodeStrings: true, delimiter: '\r\n' }));
-// parser.on('data', async (x) => {
-//     const data = await processDataString(x);
-//     console.log("Khối lượng cân được là:", data);
-// })
-
-// function processDataString(x) {
-//     return new Promise((resolve, reject) => {
-//         if (!x) reject();
-//         const a = x.split(',')[2];
-//         resolve(parseFloat(a));
-//     });
-// }
